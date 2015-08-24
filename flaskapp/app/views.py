a2d39a7c5cb71a9ae9606e1b1247d1516067cc26
RomHartmann <romhartmann@gmail.com>
2015-08-24 09:33:22 +0200

    flask app gui template added

--- flaskapp/app/views.py
+++ flaskapp/app/views.py
@@ -23,30 +23,54 @@ def upload():
         
         fFile.save(sPath)
         
-        return redirect(url_for('returned', sInput=sPath))
+        return redirect(url_for('returned_gui', sInput=sPath))
     else:
         return render_template('upload.html')
     
     
 
 
-
 @app.route('/returned', methods=['GET', 'POST'])
 def returned():
+    "returns json format results, no markup"
+    from interpcry import cry_pipeline
     sInput = request.args.get('sInput')
+    dRet = cry_pipeline.interp_cry(sInput, iResample=None)
+    os.remove(sInput)
     
+    import json
+    return json.dumps(dRet)
+
+
+
+@app.route('/returned_gui', methods=['GET', 'POST'])
+def returned_gui():
+    """returns the results with some markup for the GUI"""
     from datetime import datetime as dt
     oBegin = dt.now()
     
     from interpcry import cry_pipeline
-    
+    sInput = request.args.get('sInput')
     dRet = cry_pipeline.interp_cry(sInput, iResample=None)
+    os.remove(sInput)
     
-    sRet = "{0}: {1}".format(dRet['lsNames'], dRet['liValues'])
+    ldRet = [{'sCry':dRet['lsNames'][i], 'sConfidence': dRet['liValues'][i]} for i in range(len(dRet['lsNames']))]
     
-    #just shows time taken.  remove when no longer wanted
-    sRet += "<br/> {0}".format(dt.now()-oBegin)
+    import json
+    sTimeDelta = str(dt.now() - oBegin)
     
     
-    return sRet
+    return render_template('results.html',
+                           sTime = sTimeDelta,
+                           sUploadURL = "/upload",
+                           ldRet = ldRet)
+
+
+
+
+
+
+
+
+
 
